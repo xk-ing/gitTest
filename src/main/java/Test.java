d125edf118aff5043e3cb3dba9781348abcadf42
xk <xk@123.com>
2022-12-16 13:42:26 +0800

    ideal git test hot-fix commit1

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello git!");
         System.out.println("hello git2");
+        System.out.println("hello git3");
     }
 }
